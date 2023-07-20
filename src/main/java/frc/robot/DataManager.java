693c2465c8e85622e56a85fecd8418ae8f7db40d
Hydro111 <hydrogenhone@gmail.com>
2023-07-20 13:56:06 -0600

    Implement all DataManager method hooks
    Rename PositionManager to DataManager

--- src/main/java/frc/robot/DataManager.java
+++ src/main/java/frc/robot/DataManager.java
@@ -3,19 +3,21 @@ package frc.robot;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 
-/** Stores all of the data that is shared between systems, especially positions
+/** <b>Stores all of the data that is shared between systems, especially positions.</b>
  * 
- * Note that the entries in this class should be responsible for getting the correct information 
+ * <p>Note that the entries in this class should be responsible for getting the correct information 
  * at the correct time. The classes from which this takes information should never interact 
  * directly with classes which use their information. Most processing of the information provided
- * by systems should be done in this class.
+ * by systems should be done in this class.</p>
  * 
- * @apiNote This class has method calls in {@link Robot}
+ * <p>Also, it's possible you'll have to write entries that don't fit the {@link Entry} interface;
+ * this is ok.</p>
+ * 
+ * @implNote This class has method calls in {@link Robot}
  * 
  * @author H!
  */
-public class PositionManager {
-
+public class DataManager {
     //#########################################################
     //                    ENTRY FRAMEWORK
     //#########################################################
@@ -44,7 +46,7 @@ public class PositionManager {
     }
 
     /** An entry that does not change throughout time
-     * 
+     * @author H!
      */
     public static class StaticEntry<T> implements Entry<T> {
         protected final T m_value;
@@ -100,16 +102,47 @@ public class PositionManager {
     public static CurrentRobotPose currentRobotPose;
 
     //#########################################################
-    //                     INITIALIZATION
+    //               INITIALIZATION AND RUNTIME
     //#########################################################
 
-    /**Called after robot container is done being constructed.
+    /**Called after robot container is done being constructed. This happens
+     * when the robot code is deployed.
      * 
      * Put all construction of entries here, instead of in the definition. This
      * ensures we have control over when those entries aare constructed. You may want
      * to put some further initialization in the folowing methods, called at different times.
      */
-    public static void onSystemConstruction() {
+    public static void onRobotInit() {
         currentRobotPose = new CurrentRobotPose();
     }
+
+    /** Called when the robot is enabled. */
+    public static void onEnable() {}
+
+    /** Called when the robot is disabled, from {@link Robot#disabledInit()} */
+    public static void onDisable() {}
+
+    /** Called when the robot is enabled in teleop, from {@link Robot#teleopInit()} */
+    public static void onTeleopInit() {
+        onEnable();
+    }
+
+    /** Called when the robot is enabled in autonomous, from {@link Robot#autonomousInit()} */
+    public static void onAutoInit() {
+        onEnable();
+    }
+
+    /** Called when the robot is enabled in test mode, from {@link Robot#testInit()} */
+    public static void onTestInit() {
+        onEnable();
+    }
+
+    /** Called when the robot is created during a simulation, from {@link Robot#simulationInit()} */
+    public static void onSimulationInit() {}
+
+    /**Called every 20 milliseconds, from {@link Robot#robotPeriodic()}. Try to avoid using this method
+     * if you can, there's a decent chance if you're using it this is really something that should
+     * be done by a subsystem or command.
+     */
+    public static void periodic() {}
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -29,6 +29,7 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 
+    DataManager.onRobotInit();
   }
 
   /**
@@ -45,6 +46,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    DataManager.periodic();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
