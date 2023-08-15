6dff15ed1e154d19bde2e18ccd1c5309e5f9195b
Bryce <utbryceh@gmail.com>
2023-08-15 13:50:47 -0600

    rewritten manual control driving (with included global rate limits)

--- src/main/java/frc/robot/subsystems/SubsystemSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/SubsystemSwerveDrivetrain.java
@@ -4,9 +4,10 @@
 
 package frc.robot.subsystems;
 
-import javax.naming.OperationNotSupportedException;
-
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
@@ -16,6 +17,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.DataManager;
 import frc.robot.IMUWrapper;
 import frc.robot.Constants.DriveTrain.DriveConstants;
+import frc.robot.Constants.DriveTrain.DriveConstants.AutoConstants;
+import frc.robot.utility.TranslationRateLimiter;
 
 /**
  * Subsystem for a swerve drivetrain
@@ -37,6 +40,23 @@ public class SubsystemSwerveDrivetrain extends SubsystemBase {
   private final SubsystemSwerveModule m_rearRight = new SubsystemSwerveModule(DriveConstants.IDs.kRearRightDrivingCanId,
     DriveConstants.IDs.kRearRightTurningCanId, DriveConstants.ModuleOffsets.kBackRightOffset);
 
+  // :3 driving speeds/information
+  private Translation2d m_speeds;
+  private Boolean m_drivingFieldRelative;
+  private Double m_turnSpeedRadians;
+
+  private Translation2d m_fieldSetpoint;
+  private Rotation2d m_rotationSetpoint;
+
+  // :3 rotation pid and rate limit
+  private ProfiledPIDController m_rotationPidController = AutoConstants.kTurningPID;
+  private SlewRateLimiter m_roatationLimiter =
+    new SlewRateLimiter(DriveConstants.kMaxRotationAcceleration, -DriveConstants.kMaxRotationAcceleration, 0);
+
+  // :3 driving rate limiter
+  private TranslationRateLimiter m_drivingRateLimiter =
+    new TranslationRateLimiter(new Translation2d(), DriveConstants.kMaxDrivingAcceleration);
+
   // :3 imu
   private final IMUWrapper m_imuWrapper = new IMUWrapper();
 
@@ -57,32 +77,63 @@ public class SubsystemSwerveDrivetrain extends SubsystemBase {
     resetEncoders();
   }
 
-  // :> Test
-
   @Override
   public void periodic() {
     // :3 update odometry and feed that information into DataManager
     m_odometry.update(m_imuWrapper.getYaw(), getModulePositions());
     DataManager.currentRobotPose.updateWithOdometry(m_odometry.getPoseMeters());
-  }
 
-  /**
-   * Drives the robot with raw speeds
-   *
-   * @param xSpeed        speed of the robot in the x direction (forward)
-   * @param ySpeed        speed of the robot in the y direction (sideways)
-   * @param rotation      angular rate of the robot in radians
-   * @param fieldRelative whether the provided x and y speeds are relative to the field
-   * 
-   * @author :3
-   */
-  private void driveWithRawSpeeds(double xSpeed, double ySpeed, double rot, boolean fieldRelative) {
-    // :3 adjust the inputs if field relative is true
-    // TODO: move field relative out of here
+    // :3 initialize speeds
+    double rawXSpeed = 0.0;
+    double rawYSpeed = 0.0;
+    double rawRotationSpeed = 0.0;
+    boolean fieldRelative = false;
+
+    // :3 initialize movement speeds
+    if (m_fieldSetpoint != null) {
+      // TODO: write setpoint code
+    } else if (m_speeds != null && m_drivingFieldRelative != null) {
+      rawXSpeed = m_speeds.getX();
+      rawYSpeed = m_speeds.getY();
+      fieldRelative = m_drivingFieldRelative;
+    }
+
+    // :3 initialize rotation speeds
+    if (m_rotationSetpoint != null) {
+      // TODO: write setpoint code
+    } else if (m_turnSpeedRadians != null) {
+      rawRotationSpeed = m_turnSpeedRadians;
+    }
+
+    // :3 convert to field relative speeds
+    double rawFieldRelativeXSpeed;
+    double rawFieldRelativeYSpeed;
+    if (!fieldRelative) {
+      rawFieldRelativeXSpeed = rawXSpeed * getHeading().getCos() - rawYSpeed * getHeading().getSin();
+      rawFieldRelativeYSpeed = rawXSpeed * getHeading().getSin() + rawYSpeed * getHeading().getSin();
+    } else {
+      rawFieldRelativeXSpeed = rawXSpeed;
+      rawFieldRelativeYSpeed = rawYSpeed;
+    }
+    
+    // :3 rate limit
+    rawRotationSpeed = m_roatationLimiter.calculate(rawRotationSpeed);
+    Translation2d rawFieldRelativeSpeeds = 
+      m_drivingRateLimiter.calculate(new Translation2d(rawFieldRelativeXSpeed, rawFieldRelativeYSpeed));
+    rawFieldRelativeXSpeed = rawFieldRelativeSpeeds.getX();
+    rawFieldRelativeYSpeed = rawFieldRelativeSpeeds.getY();
+
+    // :3 reset pids
+    if (m_rotationSetpoint == null) {
+      m_rotationPidController.reset(getHeading().getRadians(), rawRotationSpeed);
+    }
+    if (m_fieldSetpoint == null) {
+      // TODO: reset driving pid
+    }
+
+    // :3 get module states
     SwerveModuleState[] swerveModuleStates = DriveConstants.ChassisKinematics.kDriveKinematics.toSwerveModuleStates(
-      fieldRelative ?
-        ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, rot, getHeading())
-        : new ChassisSpeeds(xSpeed, ySpeed, rot));
+      ChassisSpeeds.fromFieldRelativeSpeeds(rawFieldRelativeXSpeed, rawFieldRelativeYSpeed, rawRotationSpeed, getHeading()));
 
     setModuleStates(swerveModuleStates);
   }
@@ -91,32 +142,49 @@ public class SubsystemSwerveDrivetrain extends SubsystemBase {
    * Sets the speeds the robot should drive at.
    * Nulls are okay and will be used as 0's.
    *
-   * @param xSpeed        speed of the robot in the x direction (forward)
-   * @param ySpeed        speed of the robot in the y direction (sideways)
+   * @param speeds a vector representing the speeds of the robot
    * @param rotationSpeed angular rate of the robot in radians
-   * @param fieldRelative whether the provided x and y speeds are relative to the field
+   * @param drivingFieldRelative whether the provided x and y speeds are relative to the field
    * 
    * @author :3
    */
-  public void drive(Double xSpeed, Double ySpeed, Double rotationSpeed, Boolean fieldRelative) {
-    // TODO: store speeds somewhere and drive in periodic
-    driveWithRawSpeeds(xSpeed, ySpeed, rotationSpeed, fieldRelative);
+  public void driveWithSpeeds(Translation2d speeds, Double rotationSpeed, Boolean drivingFieldRelative) {
+    m_speeds = speeds;
+    m_turnSpeedRadians = rotationSpeed;
+    m_drivingFieldRelative = drivingFieldRelative;
+
+    // :3 if something's being controlled with speeds, null the setpoints
+    if (speeds != null) {
+      m_fieldSetpoint = null;
+    }
+
+    if (rotationSpeed != null) {
+      m_rotationSetpoint = null;
+    }
   }
 
   /**
-   * drive the robot with a rotation setpoint
+   * drive the robot with setpoints
    *
-   * @param xSpeed        speed of the robot in the x direction (forward)
-   * @param ySpeed        speed of the robot in the y direction (sideways)
-   * @param rotation      or goal of field relative driving
+   * @param xSpeed speed of the robot in the x direction (forward)
+   * @param ySpeed speed of the robot in the y direction (sideways)
+   * @param rotation or goal of field relative driving
    * @param fieldRelative whether the provided x and y speeds are relative to the field
    * 
    * @author :3
    */
-  public void updateSetpoint(double xSpeed, double ySpeed, Rotation2d rotation, boolean fieldRelative) 
-      throws OperationNotSupportedException {
-    // TODO: actually write this
-    throw new OperationNotSupportedException();
+  public void driveWithSetpoint(Translation2d fieldSetpoint, Rotation2d rotationSetpoint) {
+    m_fieldSetpoint = fieldSetpoint;
+    m_rotationSetpoint = rotationSetpoint;
+
+    // :3 if something's being controlled with setpoints, null the speeds
+    if (fieldSetpoint != null) {
+      m_speeds = null;
+    }
+
+    if (rotationSetpoint != null) {
+      m_turnSpeedRadians = null;
+    }
   }
 
   /**
@@ -140,7 +208,7 @@ public class SubsystemSwerveDrivetrain extends SubsystemBase {
    */
   public void setModuleStates(SwerveModuleState[] desiredStates) {
     // :3 desaturate wheel speeds
-    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kDrivingSpeedDamper);
+    SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kMaxObtainableModuleSpeed);
 
     // :3 set the desired states
     m_frontLeft.setDesiredState(desiredStates[0]);
