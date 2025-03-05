3497fc778a95c290aed885b55e7ae6b6d0489876
JasperDavidson <jasperdavdsn@gmail.com>
2025-03-04 18:42:59 -0700

    Stashing changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,6 +15,7 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
@@ -91,6 +92,8 @@ public class RobotContainer {
 
     // set sensible default commands
     setDefaultCommands();
+
+    SmartDashboard.putData(subsystemClaw);
 
     // H! Set all commands in auto selector
     setAutoCommands();
